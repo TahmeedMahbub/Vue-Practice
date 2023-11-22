a98ba26da0a51d41502c43503387bfe723afb99c
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2023-11-23 00:33:00 +0600

    text and html show

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ new Vue({
         src: "https://picsum.photos/id/237/200/300",
         alt: "Doggy",
         ht: "150px",
-        wt: "500px"
+        wt: "500px",
+
+        myText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quis assumenda incidunt modi atque aliquam recusandae, distinctio voluptatibus obcaecati doloribus consequatur dignissimos aut, a consectetur soluta impedit iure dolorem eveniet.",
+        myHtml: `<table>
+                    <tr>
+                        <td>123</td>
+                        <td>====</td>
+                        <td>321</td>
+                    </tr>
+                </table>`
     }
 });
