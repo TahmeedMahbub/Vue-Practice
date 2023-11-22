98e60a3588f6064d2b293b8bfdc89e001378a62d
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2023-11-22 23:47:42 +0600

    vue bind in attribute

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 new Vue({
     el: "#app",
     data: {
-        title: "Rafid",
-        true_false: true,
-        arr_ay: ["a", "b", "c"],
-        obj_ect: [ 
-                    {
-                        name: "ABC",
-                        phone: "123"
-                    },
-                    {
-                        name: "ABC",
-                        phone: "123"
-                    }
-                ],
-        arrow_func: ()=>{
-            return 1+2;
-        }
+        src: "https://picsum.photos/id/237/200/300",
+        alt: "Doggy",
+        ht: "150px",
+        wt: "500px"
     }
 });
