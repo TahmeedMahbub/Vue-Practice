34ade761d60452bd4de0c76d2231c0cc888554a6
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2023-11-23 00:40:49 +0600

    if else-if else condition

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 new Vue({
     el: "#app",
     data: {
-        src: "https://picsum.photos/id/237/200/300",
-        alt: "Doggy",
-        ht: "150px",
-        wt: "500px",
-
-        myText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quis assumenda incidunt modi atque aliquam recusandae, distinctio voluptatibus obcaecati doloribus consequatur dignissimos aut, a consectetur soluta impedit iure dolorem eveniet.",
-        myHtml: `<table>
-                    <tr>
-                        <td>123</td>
-                        <td>====</td>
-                        <td>321</td>
-                    </tr>
-                </table>`
+        passMarks: 50,
+        myMarks : 30,
+        avgMarks: 70
     }
 });
